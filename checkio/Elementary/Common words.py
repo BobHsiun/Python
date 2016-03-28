c48163fb45567ec4082ca27a78c1602a1c4eb1c5
BobHsiun <xiong.bo@qq.com>
2016-03-28 21:48:22 +0800

    21:37:51

--- checkio/Elementary/Common words.py
+++ checkio/Elementary/Common words.py
@@ -2,9 +2,10 @@ def checkio(first, second):
     flist=first.split(",")
     slist=second.split(",")
     print(flist,slist)
-    reslut=set(flist)&set(slist)
+    reslut=list(set(flist)&set(slist))
     print(reslut)
-    r=",".join(list(reslut))
+    reslut.sort()
+    r=",".join(reslut)
     print(r)
     return r
 
@@ -13,3 +14,9 @@ if __name__ == '__main__':
     assert checkio("hello,world", "hello,earth") == "hello", "Hello"
     assert checkio("one,two,three", "four,five,six") == "", "Too different"
     assert checkio("one,two,three", "four,five,one,two,six,three") == "one,three,two", "1 2 3"
+
+#更好的解法
+# def checkio(first, second):
+#     set1 = set(first.split(','))
+#     set2 = set(second.split(','))
+#     return ",".join(sorted(set1 & set2))
