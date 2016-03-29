23094980c65e37dabea2ef9715cd88c2e8d66fe4
BobHsiun <xiong.bo@qq.com>
2016-03-29 13:45:58 +0800

    bs4 BeautifulSoup学习

--- Spider/Test/test_urllib.py
+++ Spider/Test/test_urllib.py
@@ -1,8 +1,8 @@
-import urllib
+import urllib.request     #Python3.X的网络处理包
 url = "http://www.baidu.com"
 print("第一种方法")
 
-response1 =urllib.urlopen(url)
+response1 =urllib.request.urlopen(url)
 #直接请求
 print(response1.getcode())
 #获取状态码，如果是200表示获取成功
