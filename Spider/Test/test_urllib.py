5b1379ac7cbaaaca1d382765db614f891140ac76
BobHsiun <xiong.bo@qq.com>
2016-03-29 12:17:48 +0800

    2016-3-29 12:17:44

--- Spider/Test/test_urllib.py
+++ Spider/Test/test_urllib.py
@@ -1,8 +1,8 @@
-import urllib2
+import urllib
 url = "http://www.baidu.com"
 print("第一种方法")
 
-response1 = urllib.urlopen(url)
+response1 =urllib.urlopen(url)
 #直接请求
 print(response1.getcode())
 #获取状态码，如果是200表示获取成功
