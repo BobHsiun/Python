557055f1a3f41c2c86774015f4d809d02521f17f
BobHsiun <xiong.bo@qq.com>
2016-03-31 18:30:02 +0800

    1

--- Spider/baike_spider/html_outputer.py
+++ Spider/baike_spider/html_outputer.py
@@ -10,19 +10,19 @@ class HtmlOutputer(object):
 
     def output_html(self):
         fout = open('output.html','w',encoding="UTF-8")
-        fout.write("<html>")
+        fout.write("<html>\n")
         fout.write("<head><meta charset='UTF-8'></head>\n")
-        fout.write("<body>")
-        fout.write("<table  border=1>")
+        fout.write("<body>\n")
+        fout.write("<table  border=1>\n")
 
         for data in self.datas:
-            fout.write("<tr>")
-            fout.write("<td>%s</td>" % data['url'])
-            fout.write("<td>%s</td>" % data['title'])
-            fout.write("<td>%s</td>" % data['summary'])
-            fout.write("</tr>")
+            fout.write("    <tr>\n")
+            fout.write("        <td>%s</td>\n" % data['url'])
+            fout.write("        <td>%s</td>\n" % data['title'])
+            fout.write("        <td>%s</td>\n" % data['summary'])
+            fout.write("    </tr>\n")
 
-        fout.write("</table>")
-        fout.write("</body>")
+        fout.write("</table>\n")
+        fout.write("</body>\n")
         fout.write("</html>")
         fout.close()
