dd9b005e4c04cb35f5ceb7054567478c9431080e
Bob Hsiun <xiong.bo@qq.com>
2016-04-22 17:23:51 +0800

    2016年4月22日
    and和or的运算！

--- checkio/Electronic-station/Univocalic-factaral.py
+++ checkio/Electronic-station/Univocalic-factaral.py
@@ -1,12 +1,2 @@
-f = lambda n :n*f(n-1) if n else 1
-a_factaral = f
-print(f(10))
-
-# this assertion should be stripped after self-testing.
-if __name__ == '__main__':
-    assert a_factaral(0) == 1, "Zero"
-    assert a_factaral(1) == 1, "One"
-    assert a_factaral(2) == 2, "Two"
-    assert a_factaral(3) == 6, "Six"
-    assert a_factaral(100) == \
-           93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000, "Infinity"
+a_factaral=f=lambda x:(x>1 and x*f(x-1)-1)+1
+print(f(0),f(1),f(2),f(3),f(8),f(10))
