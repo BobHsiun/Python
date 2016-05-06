851c030a4395b28ec9839bba70c53ed83d0d90ca
Bob Hsiun <xiong.bo@qq.com>
2016-05-06 18:30:41 +0800

    save

--- mysite/functional_tests.py
+++ mysite/functional_tests.py
@@ -33,6 +33,8 @@ class NewVistorTest(unittest.TestCase):
         # 待办事项表格中显示了“1：Buy peacock feather”
         inputbox.send_keys(Keys.ENTER)
 
+        # import time
+        # time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(any(row.text == '1:Buy peacock feathers' for row in rows),"New to-do item did not appear in table")
