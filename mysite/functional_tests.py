437963043e3fb4bcd63dac101ef73b403a6c0ed7
Bob Hsiun <xiong.bo@qq.com>
2016-05-07 16:32:25 +0800

    Study TTD
    重构function test

--- mysite/functional_tests.py
+++ mysite/functional_tests.py
@@ -11,6 +11,11 @@ class NewVistorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_talbe(self,row_text):
+        table = self.browser.find_elements_by_id('id_list_talbe')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
@@ -32,12 +37,7 @@ class NewVistorTest(unittest.TestCase):
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了“1：Buy peacock feather”
         inputbox.send_keys(Keys.ENTER)
-
-        # import time
-        # time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        self.check_for_row_in_list_talbe('1:Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feather to make a fly”
@@ -48,10 +48,9 @@ class NewVistorTest(unittest.TestCase):
 
 
         # 页面再次更新，她的清单中显示了这两个待办事项
-        table = self.browser.find_element_by_id('id_list_talbe')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-        self.assertIn('2:Use peacock feathers to make a fly',[row.text for row in rows])
+        self.check_for_row_in_list_talbe('1:Buy peacock feathers')
+        self.check_for_row_in_list_talbe('2:Use peacock feathers to make a fly')
+
         # 伊迪丝想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
         # 而且页面中有一些文字解说这个功能
