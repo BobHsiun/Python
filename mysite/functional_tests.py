b25b352356e4f27033df7f4b1257519874ef894d
Bob Hsiun <xiong.bo@qq.com>
2016-05-07 16:46:06 +0800

    修改functional_tests
    elements和element的区别

--- mysite/functional_tests.py
+++ mysite/functional_tests.py
@@ -12,8 +12,8 @@ class NewVistorTest(unittest.TestCase):
         self.browser.quit()
 
     def check_for_row_in_list_talbe(self,row_text):
-        table = self.browser.find_elements_by_id('id_list_talbe')
-        rows = table.find_elements_by_tag_name('tr')
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('td')
         self.assertIn(row_text,[row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
