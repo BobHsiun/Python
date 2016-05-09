8c860da2293401ea883d48bc2d303898820ec0a5
Bob Hsiun <xiong.bo@qq.com>
2016-05-09 22:44:33 +0800

    Study TTD
    staged

--- mysite/lists/tests.py
+++ mysite/lists/tests.py
@@ -2,15 +2,14 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from .models import Item
+from .models import Item,List
 
-from .views import home_page
+from .views import home_page,new_list
 
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_view(self):
         found = resolve('/')
-        print(found.func, home_page)
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
@@ -18,8 +17,6 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         expected_html = render_to_string('lists/home.html', request=request)
-        # print(response.content.decode(),response)
-        # print(expected_html)
         self.assertEqual(response.content.decode(), expected_html)
 
     def test_home_page_can_save_a_POST_request(self):
@@ -27,7 +24,7 @@ class HomePageTest(TestCase):
         request.method = "POST"
         request.POST['item_text'] = 'A new list item'
 
-        response = home_page(request)
+        response = new_list(request)
 
         self.assertEqual(Item.objects.all().count(), 1)
         new_item = Item.objects.all()[0]
@@ -38,7 +35,7 @@ class HomePageTest(TestCase):
         request.method = "POST"
         request.POST['item_text'] = 'A new list item'
 
-        response = home_page(request)
+        response = new_list(request)
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
@@ -49,14 +46,19 @@ class HomePageTest(TestCase):
     #     self.assertEqual(Item.objects.count(), 0)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List.objects.create()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
 
         saved_items = Item.objects.all()
@@ -65,7 +67,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list,list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list,list_)
 
 
 class ListViewTest(TestCase):
@@ -86,8 +90,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'lists/list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1',list=list_)
+        Item.objects.create(text='itemey 2',list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
--- mysite/lists/views.py
+++ mysite/lists/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Item
+from .models import Item, List
 
 
 def home_page(request):
     return render(request, 'lists/home.html')
 
+
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'lists/list.html',dictionary={'items':items})
+    return render(request, 'lists/list.html', dictionary={'items': items})
+
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
