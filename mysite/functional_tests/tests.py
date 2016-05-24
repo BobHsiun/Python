abb77818a0755fcee9723b3b516194de2175f11b
Bob Hsiun <xiong.bo@qq.com>
2016-05-24 23:27:07 +0800

    Study TDD
    Use Bootstrap to improve layout

--- mysite/functional_tests/tests.py
+++ mysite/functional_tests/tests.py
@@ -1,10 +1,11 @@
-from django.test import LiveServerTestCase
+#from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
 
-class NewVistorTest(LiveServerTestCase):
+class NewVistorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -89,16 +90,16 @@ class NewVistorTest(LiveServerTestCase):
     def test_layout_and_styling(self):
         #伊迪丝访问首页
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024,768)
+        self.browser.set_window_size(1440,768)
 
         #她看到输入框完美地居中显示
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x']+inputbox.size['width']/2,512,delta=5)
+        self.assertAlmostEqual(inputbox.location['x']+inputbox.size['width']/2,720,delta=5)
 
         #她新建了一个清单,她看到输入框完美地居中显示
         inputbox.send_keys('testing\n')
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 720, delta=5)
 
 
 
