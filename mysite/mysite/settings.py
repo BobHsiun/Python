4ce0f2cc8f7f1ff58e6ae4f9305b7e26ec4d9d1e
Bob Hsiun <xiong.bo@qq.com>
2016-05-24 23:40:39 +0800

    Study TDD
    set STATIC_ROOT in settings and disable admin

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     # 'blog',
     # 'tools',
     #'learn',
-    'polls.apps.PollsConfig',
+    #'polls.apps.PollsConfig',
     'lists',
 ]
 
@@ -127,3 +127,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR,'../static'))
